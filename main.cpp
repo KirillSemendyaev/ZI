9fc46f5e110d85bd89b53c1884f90d61b5433229
Kirill Semendyaev <semendyaev.96@gmail.com>
2017-10-06 13:14:57 +0700

    Added completed ShamirUser Fixed Euclidean algorithm

--- main.cpp
+++ main.cpp
@@ -15,15 +15,15 @@ unsigned int FastExpByMod(unsigned int a, unsigned int x, unsigned int p)
     return (unsigned int)temp;
 }
 
-vector<unsigned int> EuclideanAlgorithm(unsigned int a, unsigned int b)
+vector<int> EuclideanAlgorithm(unsigned int a, unsigned int b)
 {
     if (a < b) {
         unsigned int t = a;
         a = b;
         b = t;
     }
-    vector<unsigned int> U = {a, 1, 0}, V = {b, 0, 1}, T(3);
-    unsigned int q;
+    vector<int> U = {a, 1, 0}, V = {b, 0, 1}, T(3);
+    int q;
     while (V[0] != 0) {
         q = U[0] / V[0];
         T[0] = U[0] % V[0];
@@ -92,8 +92,60 @@ pair<unsigned int, unsigned int> pgGenerator(void)
     return pg;
 }
 
+class ShamirUser {
+private:
+    unsigned int p, c, d;
+public:
+    ShamirUser(unsigned int a) {
+        p = a;
+        random_device rd;
+        vector<int> out(3);
+        do {
+            c = ((double)rd() / ((double)rd.max() + 1.0)) * (p - 2) + 1;
+            out = EuclideanAlgorithm(c, p - 1);
+        } while (out[0] != 1);
+        d = (out[2] > 0) ? out[2] : (out[2] + p - 1);
+    }
+
+    unsigned int sendToUser(unsigned int m) {
+        return FastExpByMod(m, c, p);
+    }
+    unsigned int receiveFromUser(unsigned int m) {
+        return FastExpByMod(m, c, p);
+    }
+    unsigned int replyToUser(unsigned int m) {
+        return FastExpByMod(m, d, p);
+    }
+    unsigned int decryptMessage(unsigned int m) {
+        return FastExpByMod(m, d, p);
+    }
+    void showMeYourSecrets() {
+        cout << "c = " << c << ", d = " << d << ", p = " << p << ", (c*d)%(p-1) = " << ((long long int)c * (long long int)d) % (long long int)(p - 1) << endl;
+    }
+};
+
 int main(int argc, char** argv)
 {
+    random_device rd;
+    unsigned int p = PrimeNubmerGen(1e8, 2e9), m, x1, x2, x3, x4;
+    ShamirUser user1(p), user2(p);
+    user1.showMeYourSecrets();
+    user2.showMeYourSecrets();
+
+    m = ((double)rd() / ((double)rd.max() + 1.0)) * (p - 2) + 1;
+    cout << "Message = " << m << endl;
+
+    x1 = user1.sendToUser(m);
+    cout << "x1 = " << x1 << endl;
+
+    x2 = user2.receiveFromUser(x1);
+    cout << "x2 = " << x2 << endl;
+
+    x3 = user1.replyToUser(x2);
+    cout << "x3 = " << x3 << endl;
+
+    x4 = user2.decryptMessage(x3);
+    cout << "x4 = " << x4 << endl;
 
     return 0;
 }
